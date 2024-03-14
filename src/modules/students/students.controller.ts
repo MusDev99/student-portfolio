08a15ae5c14b98c6f164af6a5e593b6237681902
AASEA\mustaqeem <muhammadmustaqeemma@airasia.com>
2024-03-14 14:57:39 +0800

    implement exception filter

--- src/modules/students/students.controller.ts
+++ src/modules/students/students.controller.ts
@@ -1,36 +1,64 @@
-import { Controller, Get, Post, Body, Put, Delete, Param, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Delete,
+  Param,
+  Req,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { CreateStudentRequest } from 'src/dto/request/Student/CreateStudentRequest';
 import { UpdateStudentRequest } from 'src/dto/request/Student/UpdateStudentRequest';
-
 
 @Controller('students')
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
   @Get()
-  findAll(@Req() request: Request) : any[] {
-    return this.studentsService.findAll();
-
+  findAll(@Req() request: Request): void {
+    try {
+      this.studentsService.findAll();
+    } catch (error) {
+      throw new HttpException('Empty', HttpStatus.NO_CONTENT);
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) : boolean {
+  findOne(@Param('id') id: string): boolean {
     return this.studentsService.findOne(+id); // Convert string ID to number
   }
 
   @Post()
-  create(@Body() CreateStudentRequest: CreateStudentRequest) : any {
-    return this.studentsService.create(CreateStudentRequest);
+  create(@Body() CreateStudentRequest: CreateStudentRequest): any {
+    try {
+      return this.studentsService.create(CreateStudentRequest);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() UpdateStudentRequest: UpdateStudentRequest) {
-    return this.studentsService.update(+id, UpdateStudentRequest);
+  update(
+    @Param('id') id: string,
+    @Body() UpdateStudentRequest: UpdateStudentRequest,
+  ): any {
+    try {
+      return this.studentsService.update(+id, UpdateStudentRequest);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.studentsService.remove(+id);
+    try {
+      return this.studentsService.remove(+id);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
